47633447f8764e84bea4bacb2a3c40d89e0f9935
Christian Gonzalez <chrisgo@microsoft.com>
2016-09-15 10:38:49 -0700

    Add a simple html page and unminfied crash callstack that can be used to obtain sample callstacks in different browsers.

---
+++ src/SourcemapToolkit.CallstackTestApp/crashcauser.js
@@ -0,0 +1,26 @@
+﻿function level1() {
+    var longLocalVariableName = 16;
+    longLocalVariableName += 2;
+    level2(longLocalVariableName);
+
+}
+
+function level2(input) {
+    input = input + 2;
+    level3(input);
+}
+
+function level3(input) {
+    var x;
+    console.log(x.length + input);
+}
+
+window.onerror = function (message, source, lineno, colno, error) {
+    document.getElementById("callstackdisplay").innerText = error.stack;
+}
+
+window.onload = function (event) {
+    document.getElementById("crashbutton").addEventListener("click", function () {
+        level1();
+    });
+}
