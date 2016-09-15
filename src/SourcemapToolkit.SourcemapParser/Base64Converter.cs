79ac151487c465397422cc882d7b14ff32c18de2
Christian Gonzalez <chrisgo@microsoft.com>
2016-09-15 15:15:00 -0700

    Sourcemap mapping entries are encoded using Base64 VLQ. Add a Base64Converter that will handle converting Base64 values into integers.

---
+++ src/SourcemapToolkit.SourcemapParser/Base64Converter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SourcemapToolkit.SourcemapParser
+{
+	/// <summary>
+	/// Used to convert Base64 characters values into integers
+	/// </summary>
+	internal class Base64Converter
+	{
+		private const string Base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+		private readonly Dictionary<char, int> _base64DecodeMap = new Dictionary<char, int>();
+
+		public Base64Converter()
+		{
+			for (int i = 0; i < Base64Alphabet.Length; i += 1)
+			{
+				_base64DecodeMap[Base64Alphabet[i]] = i;
+			}
+		}
+
+		/// <summary>
+		/// Converts a base64 value to an integer.
+		/// </summary>
+		internal int FromBase64(char base64Value)
+		{
+			if (!_base64DecodeMap.ContainsKey(base64Value))
+			{
+				throw new ArgumentOutOfRangeException(nameof(base64Value), "Tried to convert an invalid base64 value");
+			}
+
+			return _base64DecodeMap[base64Value];
+		}
+	}
+}
---
+++ tests/SourcemapToolkit.SourcemapParser.UnitTests/Base64ConverterUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourcemapToolkit.SourcemapParser.UnitTests
+{
+	[TestClass]
+	public class Base64ConverterUnitTests
+	{
+		[TestMethod]
+		public void FromBase64_ValidBase64InputC_CorrectIntegerOutput2()
+		{
+			// Arrange
+			Base64Converter base64Converter = new Base64Converter();
+
+			// Act
+			int value = base64Converter.FromBase64('C');
+
+			// Assert
+			Assert.AreEqual(2, value);
+		}
+
+		[TestMethod]
+		public void FromBase64_ValidBase64Input9_CorrectIntegerOutput61()
+		{
+			// Arrange
+			Base64Converter base64Converter = new Base64Converter();
+
+			// Act
+			int value = base64Converter.FromBase64('9');
+
+			// Assert
+			Assert.AreEqual(61, value);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void FromBase64_InvalidBase64Input_ThrowsException()
+		{
+			// Arrange
+			Base64Converter base64Converter = new Base64Converter();
+
+			// Act
+			base64Converter.FromBase64('@');
+		}
+	}
+}
