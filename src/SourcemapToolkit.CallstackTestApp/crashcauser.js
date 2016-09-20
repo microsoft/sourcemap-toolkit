01e4ad9692016aa97bdfe407f8d4cf77b028b63a
Christian Gonzalez <christianjordangonzalez@gmail.com>
2016-09-20 16:05:26 -0700

    Ajaxmin testapp (#6)
    * Add code to parse stackframes in IE11, Chrome, and FireFox into objects that can be used by the rest of the library. In order to minimize confusion over zero based vs one based line and column numbers, this change ensures that we only ever work with zero based line and column numbers.
    
    * Minify crashcauser.js so we can get minified call stacks in the test app.

--- src/SourcemapToolkit.CallstackTestApp/crashcauser.js
+++ src/SourcemapToolkit.CallstackTestApp/crashcauser.js
@@ -1,25 +1,30 @@
-﻿function level1() {
-    var longLocalVariableName = 16;
-    longLocalVariableName += 2;
-    level2(longLocalVariableName);
-}
+﻿function causeCrash()
+{
+    function level1() {
+        var longLocalVariableName = 16;
+        longLocalVariableName += 2;
+        level2(longLocalVariableName);
+    }
 
-function level2(input) {
-    input = input + 2;
-    level3(input);
-}
+    function level2(input) {
+        input = input + 2;
+        level3(input);
+    }
 
-function level3(input) {
-    var x;
-    console.log(x.length + input);
-}
+    function level3(input) {
+        var x;
+        console.log(x.length + input);
+    }
 
-window.onerror = function (message, source, lineno, colno, error) {
-    document.getElementById("callstackdisplay").innerText = error.stack;
+    window.onerror = function (message, source, lineno, colno, error) {
+        document.getElementById("callstackdisplay").innerText = error.stack;
+    }
+
+    level1();
 }
 
 window.onload = function (event) {
     document.getElementById("crashbutton").addEventListener("click", function () {
-        level1();
+        causeCrash();
     });
 }
