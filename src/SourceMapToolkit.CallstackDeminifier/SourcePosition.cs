40311d8782414f5a2bd71628291adfcbbbafd0ed
Christian Gonzalez <christianjordangonzalez@gmail.com>
2016-09-21 11:02:03 -0700

    Function map generator (#7)
    * Implement IComparable for Sourceposition
    
    * We'll need code to be able to determine what the location of the function that wraps a given line of code in order to deminify callstacks correctly. Add code that iterates over the JavaScript AST (using AjaxMin) and creates a FunctionMapEntry object that describes the name and location of that function to the FunctionMap.

--- src/SourceMapToolkit.CallstackDeminifier/SourcePosition.cs
+++ src/SourceMapToolkit.CallstackDeminifier/SourcePosition.cs
@@ -1,12 +1,34 @@
-﻿namespace SourcemapToolkit.CallstackDeminifier
+﻿using System;
+
+namespace SourcemapToolkit.CallstackDeminifier
 {
 	/// <summary>
 	/// Identifies the location of a piece of code in a JavaScript file
 	/// </summary>
-	public class SourcePosition
+	public class SourcePosition : IComparable<SourcePosition>
 	{
 		public int ZeroBasedLineNumber;
 
 		public int ZeroBasedColumnNumber;
+
+		public int CompareTo(SourcePosition other)
+		{
+			if (this.ZeroBasedLineNumber == other.ZeroBasedLineNumber)
+			{
+				return this.ZeroBasedColumnNumber.CompareTo(other.ZeroBasedColumnNumber);
+			}
+
+			return this.ZeroBasedLineNumber.CompareTo(other.ZeroBasedLineNumber);
+		}
+
+		public static bool operator <(SourcePosition x, SourcePosition y)
+		{
+			return x.CompareTo(y) < 0;
+		}
+
+		public static bool operator >(SourcePosition x, SourcePosition y)
+		{
+			return x.CompareTo(y) > 0;
+		}
 	}
 }
---
+++ tests/SourcemapToolkit.CallstackDeminifier.UnitTests/FunctionMapGeneratorUnitTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourcemapToolkit.CallstackDeminifier.UnitTests
+{
+	[TestClass]
+	public class FunctionMapGeneratorUnitTests
+	{
+		[TestMethod]
+		public void GenerateFunctionMap_NoFunctionsInSource_EmptyFunctionList()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "bar();";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(0, functionMap.Count);
+		}
+
+		[TestMethod]
+		public void GenerateFunctionMap_SingleLineFunctionInSource_CorrectZeroBasedColumnNumbers()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "function foo(){bar();}";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(1, functionMap.Count);
+			Assert.AreEqual("foo", functionMap[0].FunctionName);
+			Assert.AreEqual(0, functionMap[0].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[0].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(14, functionMap[0].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(22, functionMap[0].EndSourcePosition.ZeroBasedColumnNumber);
+		}
+
+		[TestMethod]
+		public void GenerateFunctionMap_MultiLineFunctionInSource_CorrectColumnAndZeroBasedLineNumbers()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "function foo()" + Environment.NewLine + "{" + Environment.NewLine + "bar();" +
+								Environment.NewLine + "}";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(1, functionMap.Count);
+			Assert.AreEqual("foo", functionMap[0].FunctionName);
+			Assert.AreEqual(1, functionMap[0].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(3, functionMap[0].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[0].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(1, functionMap[0].EndSourcePosition.ZeroBasedColumnNumber);
+		}
+
+		[TestMethod]
+		public void GenerateFunctionMap_TwoSingleLineFunctions_TwoFunctionMapEntries()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "function foo(){bar();}function bar(){baz();}";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(2, functionMap.Count);
+
+			Assert.AreEqual("foo", functionMap[0].FunctionName);
+			Assert.AreEqual(0, functionMap[0].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[0].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(14, functionMap[0].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(22, functionMap[0].EndSourcePosition.ZeroBasedColumnNumber);
+
+			Assert.AreEqual("bar", functionMap[1].FunctionName);
+			Assert.AreEqual(0, functionMap[1].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[1].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(36, functionMap[1].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(44, functionMap[1].EndSourcePosition.ZeroBasedColumnNumber);
+		}
+
+		[TestMethod]
+		public void GenerateFunctionMap_TwoNestedSingleLineFunctions_TwoFunctionMapEntries()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "function foo(){function bar(){baz();}}";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(2, functionMap.Count);
+
+			Assert.AreEqual("foo", functionMap[0].FunctionName);
+			Assert.AreEqual(0, functionMap[0].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[0].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(14, functionMap[0].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(38, functionMap[0].EndSourcePosition.ZeroBasedColumnNumber);
+
+			Assert.AreEqual("bar", functionMap[1].FunctionName);
+			Assert.AreEqual(0, functionMap[1].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[1].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(29, functionMap[1].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(37, functionMap[1].EndSourcePosition.ZeroBasedColumnNumber);
+		}
+	}
+}
