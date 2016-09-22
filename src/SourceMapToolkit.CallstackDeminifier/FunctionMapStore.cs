d02ae7276a02fdce501b3d80f545313e32adbc51
Christian Gonzalez <chrisgo@microsoft.com>
2016-09-22 11:56:54 -0700

    Implement the FunctionMapStore and SourceMapStore which will be the main API for obtaining source maps and function maps. The source maps and function maps are generated by obtaining the contents of the source map/code file from the consumer of the API. Once the source map and function map has been generated, the value is cached for future use.

---
+++ src/SourceMapToolkit.CallstackDeminifier/FunctionMapStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SourcemapToolkit.CallstackDeminifier
+{
+	internal interface IFunctionMapStore
+	{
+		List<FunctionMapEntry> GetFunctionMapForSourceCode(string sourceCodeUrl);
+	}
+
+	/// <summary>
+	/// This class is responsible for providing function maps for the JavaScript code corresponding to a URL.
+	/// </summary>
+	internal class FunctionMapStore : IFunctionMapStore
+	{
+		private readonly ISourceCodeProvider _sourceCodeProvider;
+		private readonly IFunctionMapGenerator _functionMapGenerator;
+		private readonly KeyValueCache<string,List<FunctionMapEntry>> _functionMapCache;
+
+		public FunctionMapStore(ISourceCodeProvider sourceCodeProvider)
+		{
+			_sourceCodeProvider = sourceCodeProvider;
+			_functionMapGenerator = new FunctionMapGenerator();
+			_functionMapCache = new KeyValueCache<string, List<FunctionMapEntry>>();
+		}
+
+		/// <summary>
+		/// Returns a function map for the given URL. If the function map is not already available,
+		/// it is obtained from the API consumer using the provided ISourceCodeProvider.
+		/// Once a function map is generated, the value is cached in memory for future usages.
+		/// </summary>
+		/// <param name="sourceCodeUrl">The URL of the file for which a function map is required</param>
+		public List<FunctionMapEntry> GetFunctionMapForSourceCode(string sourceCodeUrl)
+		{
+			return _functionMapCache.GetValue(sourceCodeUrl,
+				() => _functionMapGenerator.GenerateFunctionMap(
+						_sourceCodeProvider.GetSourceCode(sourceCodeUrl)));
+		}
+	}
+}
--- src/SourceMapToolkit.CallstackDeminifier/ISourceCodeProvider.cs
+++ src/SourceMapToolkit.CallstackDeminifier/ISourceCodeProvider.cs
@@ -4,13 +4,13 @@
 	/// This class is to be implemented by the consumer of the source map library.
 	/// The implementation should be able to return the contents of a file referenced in the stack frame URL.
 	/// </summary>
-	public interface IGeneratedSourceCodeProvider
+	public interface ISourceCodeProvider
 	{
 		/// <summary>
 		/// This method will be invoked for each unique genered code URL that appears in a call stack in order to retreieve 
 		/// the contents of that file.
 		/// </summary>
-		/// <param name="correspondingCallStackFileUrl">The url from a callstack stackframe that caused us to invoke this method</param>
-		string GetSourceCodeForStackFrame(string correspondingCallStackFileUrl);
+		/// <param name="sourceCodeUrl">The url from a callstack stackframe that caused us to invoke this method</param>
+		string GetSourceCode(string sourceCodeUrl);
 	}
 }
