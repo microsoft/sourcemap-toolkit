96534ed37076a7afe6d0d337e849d8dbb749b0e7
Christian Gonzalez <chrisgo@microsoft.com>
2016-09-22 11:22:10 -0700

    Create interfaces for getting the generated code and source map from the consumer of the API

---
+++ src/SourceMapToolkit.CallstackDeminifier/IGeneratedSourceCodeProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace SourcemapToolkit.CallstackDeminifier
+{
+	/// <summary>
+	/// This class is to be implemented by the consumer of the source map library.
+	/// The implementation should be able to return the contents of a file referenced in the stack frame URL.
+	/// </summary>
+	public interface IGeneratedSourceCodeProvider
+	{
+		/// <summary>
+		/// This method will be invoked for each unique genered code URL that appears in a call stack in order to retreieve 
+		/// the contents of that file.
+		/// </summary>
+		/// <param name="correspondingCallStackFileUrl">The url from a callstack stackframe that caused us to invoke this method</param>
+		string GetSourceCodeForStackFrame(string correspondingCallStackFileUrl);
+	}
+}
