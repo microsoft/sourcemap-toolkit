04a462f6f19db4c61323bbb9d2ee0b7ea8215310
Christian Gonzalez <christianjordangonzalez@gmail.com>
2016-09-29 11:15:40 -0700

    Move MappingEntry to it's own file.

--- src/SourcemapToolkit.SourcemapParser/SourceMap.cs
+++ src/SourcemapToolkit.SourcemapParser/SourceMap.cs
@@ -52,27 +52,4 @@ namespace SourcemapToolkit.SourcemapParser
 	        return null;
 	    }
 	}
-
-	public class MappingEntry
-	{
-		/// <summary>
-		/// The location of the line of code in the transformed code
-		/// </summary>
-		public SourcePosition GeneratedSourcePosition;
-
-		/// <summary>
-		/// The location of the code in the original source code
-		/// </summary>
-		public SourcePosition OriginalSourcePosition;
-
-		/// <summary>
-		/// The original name of the code referenced by this mapping entry
-		/// </summary>
-		public string OriginalName;
-
-		/// <summary>
-		/// The name of the file that originally contained this code
-		/// </summary>
-		public string OriginalFileName;
-	}
 }
