bbb260a0cde131bff643f7fb06d64ec8ee880aef
Christian Gonzalez <chrisgo@microsoft.com>
2016-10-10 15:05:58 -0700

    In the event that a function is bound to a property name or variable and has a function name (for example var foo = function bar() {}), we should ignore the function's name and instead take note of the name and position of the binding (we'd take foo and not bar in the example). This is to match what will appear in the callstack and also takes into account the fact that some minifiers would strip the function name in this scenario.

--- src/SourceMapToolkit.CallstackDeminifier/FunctionFinderVisitor.cs
+++ src/SourceMapToolkit.CallstackDeminifier/FunctionFinderVisitor.cs
@@ -11,59 +11,25 @@ namespace SourcemapToolkit.CallstackDeminifier
 	/// </summary>
 	internal class FunctionFinderVisitor : TreeVisitor
 	{
+		private class FunctionNameInformation
+		{
+			public string FunctionName;
+			public SourcePosition FunctionNameSourcePosition;
+		}
+
 		internal readonly List<FunctionMapEntry> FunctionMap = new List<FunctionMapEntry>();
 
 		public override void Visit(FunctionObject node)
 		{
 			base.Visit(node);
-			string functionName = null;
-			SourcePosition functionNameSourcePosition = null;
+			FunctionNameInformation functionNameInformation = GetFunctionNameInformation(node);
 
-			BindingIdentifier bindingIdentifier = node.Binding;
-
-			BinaryOperator parentBinaryOperator = node.Parent as BinaryOperator;
-			ObjectLiteralProperty parentObjectLiteralProperty = node.Parent as ObjectLiteralProperty;
-
-			if (bindingIdentifier == null && node.Parent is VariableDeclaration)
-			{
-				bindingIdentifier = ((VariableDeclaration)node.Parent).Binding as BindingIdentifier;
-			}
-
-			if (bindingIdentifier != null)
-			{
-				functionName = bindingIdentifier.Name;
-				functionNameSourcePosition = new SourcePosition
-				{
-					ZeroBasedLineNumber = bindingIdentifier.Context.StartLineNumber - 1,
-					// Souce maps work with zero based line and column numbers, the AST works with one based line numbers. We want to use zero-based everywhere.
-					ZeroBasedColumnNumber = bindingIdentifier.Context.StartColumn
-				};
-			}
-			else if (parentBinaryOperator != null)
-			{
-				functionName = parentBinaryOperator.Operand1.Context.Code;
-				functionNameSourcePosition = new SourcePosition
-				{
-					ZeroBasedLineNumber = parentBinaryOperator.Operand1.Context.StartLineNumber - 1,
-					ZeroBasedColumnNumber = parentBinaryOperator.Operand1.Context.StartColumn
-				};
-			}
-			else if (parentObjectLiteralProperty != null)
-			{
-				functionName = parentObjectLiteralProperty.Name.Name;
-				functionNameSourcePosition = new SourcePosition
-				{
-					ZeroBasedLineNumber = parentObjectLiteralProperty.Context.StartLineNumber - 1,
-					ZeroBasedColumnNumber = parentObjectLiteralProperty.Context.StartColumn
-				};
-			}
-
-			if (functionNameSourcePosition != null)
+			if (functionNameInformation != null)
 			{
 				FunctionMapEntry functionMapEntry = new FunctionMapEntry
 				{
-					FunctionName = functionName,
-					FunctionNameSourcePosition = functionNameSourcePosition,
+					FunctionName = functionNameInformation.FunctionName,
+					FunctionNameSourcePosition = functionNameInformation.FunctionNameSourcePosition,
 					StartSourcePosition = new SourcePosition
 					{
 						ZeroBasedLineNumber = node.Body.Context.StartLineNumber - 1, // Souce maps work with zero based line and column numbers, the AST works with one based line numbers. We want to use zero-based everywhere.
@@ -78,6 +44,72 @@ namespace SourcemapToolkit.CallstackDeminifier
 
 				FunctionMap.Add(functionMapEntry);
 			}	
+		}
+
+		/// <summary>
+		/// Gets the name and location information related to the function name binding for a FunctionObject node
+		/// </summary>
+		private FunctionNameInformation GetFunctionNameInformation(FunctionObject node)
+		{
+			// Gets the name of an object property that a function is bound to, like foo in the example "object.foo = function () {}"
+			BinaryOperator parentBinaryOperator = node.Parent as BinaryOperator;
+			if (parentBinaryOperator != null)
+			{
+				return new FunctionNameInformation
+				{
+					FunctionName = parentBinaryOperator.Operand1.Context.Code,
+					FunctionNameSourcePosition = new SourcePosition
+					{
+						ZeroBasedLineNumber = parentBinaryOperator.Operand1.Context.StartLineNumber - 1,
+						ZeroBasedColumnNumber = parentBinaryOperator.Operand1.Context.StartColumn
+					}
+				};
+			}
+
+			// Gets the name of an object property that a function is bound to against the prototype, like foo in the example "object.prototype = {foo: function () {}}"
+			ObjectLiteralProperty parentObjectLiteralProperty = node.Parent as ObjectLiteralProperty;
+			if (parentObjectLiteralProperty != null)
+			{
+				return new FunctionNameInformation
+				{
+					FunctionName = parentObjectLiteralProperty.Name.Name,
+					FunctionNameSourcePosition = new SourcePosition
+					{
+						ZeroBasedLineNumber = parentObjectLiteralProperty.Context.StartLineNumber - 1,
+						ZeroBasedColumnNumber = parentObjectLiteralProperty.Context.StartColumn
+					}
+				};
+			}
+
+			BindingIdentifier bindingIdentifier = null;
+
+			// Gets the name of a variable that a function is bound to, like foo in the example "var foo = function () {}"
+			VariableDeclaration parentVariableDeclaration = node.Parent as VariableDeclaration;
+			if (parentVariableDeclaration != null)
+			{
+				bindingIdentifier = parentVariableDeclaration.Binding as BindingIdentifier;
+			}
+			// Gets the name bound to the function, like foo in the example "function foo() {}
+			else
+			{
+				bindingIdentifier = node.Binding;
+			}
+
+			if (bindingIdentifier != null)
+			{
+				return new FunctionNameInformation
+				{
+					FunctionName = bindingIdentifier.Name,
+					FunctionNameSourcePosition = new SourcePosition
+					{
+						ZeroBasedLineNumber = bindingIdentifier.Context.StartLineNumber - 1,
+						// Souce maps work with zero based line and column numbers, the AST works with one based line numbers. We want to use zero-based everywhere.
+						ZeroBasedColumnNumber = bindingIdentifier.Context.StartColumn
+					}
+				};
+			}
+
+			return null;
 		}
 	}
 }
--- tests/SourcemapToolkit.CallstackDeminifier.UnitTests/FunctionMapGeneratorUnitTests.cs
+++ tests/SourcemapToolkit.CallstackDeminifier.UnitTests/FunctionMapGeneratorUnitTests.cs
@@ -205,5 +205,87 @@ namespace SourcemapToolkit.CallstackDeminifier.UnitTests
 			Assert.AreEqual(20, functionMap[1].StartSourcePosition.ZeroBasedColumnNumber);
 			Assert.AreEqual(22, functionMap[1].EndSourcePosition.ZeroBasedColumnNumber);
 		}
+
+		[TestMethod]
+		public void GenerateFunctionMap_FunctionAssignedToVariableAndHasName_FunctionMapEntryGeneratedForVariableName()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "var foo = function myCoolFunctionName(){ bar(); }";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(1, functionMap.Count);
+
+			Assert.AreEqual("foo", functionMap[0].FunctionName);
+			Assert.AreEqual(0, functionMap[0].FunctionNameSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(4, functionMap[0].FunctionNameSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(0, functionMap[0].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[0].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(39, functionMap[0].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(49, functionMap[0].EndSourcePosition.ZeroBasedColumnNumber);
+		}
+
+		[TestMethod]
+		public void GenerateFunctionMap_FunctionAssignedToObjectPropertyAndHasName_FunctionMapEntryGeneratedForPropertyName()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "var foo = function(){};foo.bar = function myCoolFunctionName() { baz(); }";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(2, functionMap.Count);
+
+			Assert.AreEqual("foo.bar", functionMap[0].FunctionName);
+			Assert.AreEqual(0, functionMap[0].FunctionNameSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(23, functionMap[0].FunctionNameSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(0, functionMap[0].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[0].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(63, functionMap[0].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(73, functionMap[0].EndSourcePosition.ZeroBasedColumnNumber);
+
+			Assert.AreEqual("foo", functionMap[1].FunctionName);
+			Assert.AreEqual(0, functionMap[1].FunctionNameSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(4, functionMap[1].FunctionNameSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(0, functionMap[1].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[1].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(20, functionMap[1].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(22, functionMap[1].EndSourcePosition.ZeroBasedColumnNumber);
+		}
+
+		[TestMethod]
+		public void GenerateFunctionMap_FunctionAssignedToObjectPrototypeAndHasName_FunctionMapEntryGeneratedForObjectPrototype()
+		{
+			// Arrange
+			IFunctionMapGenerator functionMapGenerator = new FunctionMapGenerator();
+			string sourceCode = "var foo = function(){} foo.prototype = { bar: function myCoolFunctionName() { baz(); } }";
+
+			// Act
+			List<FunctionMapEntry> functionMap = functionMapGenerator.GenerateFunctionMap(sourceCode);
+
+			// Assert
+			Assert.AreEqual(2, functionMap.Count);
+
+			Assert.AreEqual("bar", functionMap[0].FunctionName);
+			Assert.AreEqual(0, functionMap[0].FunctionNameSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(41, functionMap[0].FunctionNameSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(0, functionMap[0].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[0].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(76, functionMap[0].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(86, functionMap[0].EndSourcePosition.ZeroBasedColumnNumber);
+
+			Assert.AreEqual("foo", functionMap[1].FunctionName);
+			Assert.AreEqual(0, functionMap[1].FunctionNameSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(4, functionMap[1].FunctionNameSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(0, functionMap[1].StartSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(0, functionMap[1].EndSourcePosition.ZeroBasedLineNumber);
+			Assert.AreEqual(20, functionMap[1].StartSourcePosition.ZeroBasedColumnNumber);
+			Assert.AreEqual(22, functionMap[1].EndSourcePosition.ZeroBasedColumnNumber);
+		}
 	}
 }
